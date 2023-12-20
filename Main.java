7c9b9af9aa91355ba332899bb767c66d5d611ce9
GauharRepo <tech.mdg11@gmail.com>
2023-12-20 13:07:05 +0530

    added student class

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main{
         System.out.println("output from java program");
         System.out.println("added one more comment");
         System.out.println("added second comment");
+
+        Student student=new Student();
+        student.sayHello();
         System.out.println("end");
     }
 }
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    public Student(){
+        System.out.println("student class");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi i am student!");
+    }
+}
